65e529ca7e3a7ba49ec03df9a8726fd5f57572b7
Jackson Barreto <jackson.barreto@gmail.com>
2021-02-23 18:09:26 +0000

    created accountant and profile

---
+++ src/components/Profile.tsx
@@ -0,0 +1,16 @@
+import styles from '../styles/components/Profile.module.css';
+
+export function Profile() {
+    return (
+        <div className={styles.profileContainer}>
+            <img src="https://avatars.githubusercontent.com/u/13190121?s=460&v=4" alt="Jackson Barreto" />
+            <div>
+                <strong>Jackson Barreto</strong>
+                <p>
+                    <img src="icons/level.svg" alt="Level" />
+                    Level 1
+                </p>
+            </div>
+        </div>
+    );
+}
